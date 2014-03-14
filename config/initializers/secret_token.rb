c1be4a7d6787541b13823879f57ef3f868f76b31
yunjie.zhang <yunjie.zhang@drecom.co.jp>
2014-03-15 02:32:42 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrazyStudio::Application.config.secret_key_base = 'be7dd55b3b8ac94c3f187e85d8fa36a46f7deaa33ca1a645c4d6322180ddc04baf652760629873568d19bf9d910ea03f2722890d3da584d4faaa72c43688fdf3'
